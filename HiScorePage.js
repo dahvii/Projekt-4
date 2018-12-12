ee4d7f0d9764ae6b996d0c79d84a78b207b8ebe0
Louise Joy Frahm <louisejoyfrahm@mail.dk>
2018-12-12 20:05:11 +0100

    Worked a bit on the highscore

--- HiScorePage.js
+++
@@ -1,8 +0,0 @@
-class HiScorePage extends Component {
-
-    constructor(){
-      super();
-      this.addRoute('/hiScore', 'HiScore');
-    }
-  }
-  //Hente ud arrayen og sortere den alt efter moves og skrive ud på siden 
--- www/js/HiScorePage.js
+++ www/js/HiScorePage.js
@@ -1,9 +1,26 @@
 class HiScorePage extends Component {
 
-    constructor(){
-      super();
-      this.addRoute('/hiScore', 'HiScore');
-    }
-  
-  
+  constructor() {
+    super();
+    this.addRoute('/hiScore', 'HiScore');
+    const highscore = localStorage.getItem('highscore');
+    this.highscoreArray = highscore ? JSON.parse(highscore) : [];
+    // Sortera
+    this.highscoreArray = this.highscoreArray.slice().sort((a, b) => { return a.moves > b.moves ? 1 : -1; });
+    setTimeout(() => {
+      this.fillHighscore();
+    }, 100);
   }
+
+  fillHighscore() { 
+    this.highscoreArray.forEach((highscore, i) => {
+      const element = 
+      `<tr>
+        <td>${i+1}</td>
+        <td>${highscore.name}</td>
+        <td>${highscore.moves}</td>
+    </tr>`
+    $('#highScoreList').append(element)
+    })
+  }
+}
