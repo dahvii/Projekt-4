56b4bf3e90e6e655a1048f9dff6c10099ec9db59
Louise Joy Frahm <louisejoyfrahm@mail.dk>
2018-12-13 17:42:32 +0100

    Removed Formpage from navbar and changed button

--- www/js/FormPage.js
+++ www/js/FormPage.js
@@ -1,65 +1,60 @@
 class FormPage extends Component {
 
-    constructor(){
-      super();
-      this.addRoute('/gameForm', 'Game Form');
-      this.addEvents({
-        'click #start-button': 'addPlayer',
-        'click .btn-cancel': 'cancel'
-      });
+  constructor() {
+    super();
+    this.addRoute('/gameForm', 'Game Form');
+    this.addEvents({
+      'click #start-button': 'addPlayer',
+    });
+  }
+
+
+  addPlayer() {
+    $('#invalid-input1').hide();
+    $('#invalid-input2').hide();
+    const player1 = $('#name-input1').val();
+    const player2 = $('#name-input2').val();
+    const typ1 = $('#typ-1').val();
+    const typ2 = $('#typ-2').val();
+    let success = true;
+    if (this.validate(player1)) {
+      localStorage.setItem('player-1-name', player1);
+      localStorage.setItem('typ-1', typ1);
+    } else {
+      $('#invalid-input1').show();
+      success = false;
     }
 
+    if (this.validate(player2)) {
+      localStorage.setItem('player-2-name', player2);
+      localStorage.setItem('typ-2', typ2);
 
-    addPlayer(){
-      $('#invalid-input1').hide();
-      $('#invalid-input2').hide();
-      const player1 = $('#name-input1').val();
-      const player2 = $('#name-input2').val();
-      const typ1 = $('#typ-1').val();
-      const typ2 = $('#typ-2').val();
-      let success = true;
-      if (this.validate(player1)) {
-        localStorage.setItem('player-1-name', player1);
-        localStorage.setItem('typ-1', typ1);
-      } else {
+    } else {
+      $('#invalid-input2').show();
+      success = false;
+    }
+
+    if (success) {
+      if (player1 !== player2) {
+        location.href = '/game';
+      }
+
+      else {
         $('#invalid-input1').show();
-        success = false;
-      }
-
-      if (this.validate(player2)) {
-        localStorage.setItem('player-2-name', player2);
-        localStorage.setItem('typ-2', typ2);
-
-      } else {
-        $('#invalid-input2').show();
-        success = false;
-      }
-
-      if (success) {
-        if (player1!==player2){
-        location.href = '/game';}
-        
-        else{
-        $('#invalid-input1').show();
         $('#invalid-input2').show();
       }
-      }
     }
+  }
 
-    validate(name){
-      return name.length >= 2 && name.length <= 10;
-    }
-    
-     cancel(){
-      location.href = '/';
-    }
-  } 
+  validate(name) {
+    return name.length >= 2 && name.length <= 10;
+  }
+}
 
-    
 
-  
 
-    
 
-   
-  
+
+
+
+
